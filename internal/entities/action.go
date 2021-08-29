c72a0c0725533ff11729aa3754781d872a9c8638
Peppy Sisay <psisay@netflix.com>
2021-08-29 01:08:42 -0700

    Add missing json annotations

--- internal/entities/action.go
+++ internal/entities/action.go
@@ -6,11 +6,11 @@ const (
 )
 
 type Action struct {
-	ID          string
-	Description string
+	ID          string `json:"id"`
+	Description string `json:"description"`
 }
 
 type ActionResult struct {
-	Message  string
-	Metadata map[string]interface{}
+	Message  string                 `json:"message"`
+	Metadata map[string]interface{} `json:"metadata"`
 }
